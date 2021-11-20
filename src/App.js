1694833b52b15834d9036e8911336ab87259e447
Monty Hood <montyhood@gmail.com>
2021-11-20 08:21:18 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://monty.net/gratitude"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gratitude card on monty.net/gratitude
-        </a>
-      </header>
+    <div class="container">
+    <img class="exampleimage" src="https://files.outfit.io/media_library_items/349582/gratitude-bg-option1-withtext.png" />
+        <div>
+
+              <div class="row">
+                <div class="column"><input type="radio" name="backgroundimage" value="https://files.outfit.io/media_library_items/349584/gratitude-bg-option1.png" checked />Option 1</div>
+                <div class="column"><input type="radio" name="backgroundimage" value="https://files.outfit.io/media_library_items/349583/gratitude-bg-option2.png" />Option 2</div>
+              </div>
+
+    <input type="text" maxlength="60" id="page" class="text-field" placeholder="Enter message here (60 characters maximum)" onfocus="this.placeholder = ''" />
+        </div>
+
+        <div>
+            <input type="submit" class="submit-button" value="submit" onclick="goToPage(); displayText();" />
+        </div>
+    <div id="smalltext"></div>
+    </div>
     </div>
   );
 }
