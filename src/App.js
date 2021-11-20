76bf506c1f88bb216cc9326ce302e6af8e80ee9f
Monty Hood <montyhood@gmail.com>
2021-11-20 09:03:28 -0500

    Edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Gratitude card on monty.net/gratitude
+          monty.net/gratitude
         </a>
       </header>
     </div>
