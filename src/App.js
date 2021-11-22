98d5c5297e34daeb194dff664688351dbf1e049f
Monty Hood <montyhood@gmail.com>
2021-11-22 14:31:27 -0500

    updating for temp deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +6,7 @@ function App() {
       <header className="App-header">
         <img src="https://files.outfit.io/media_library_items/349582/gratitude-bg-option1-withtext.png" />
         <p>
-          This will be live shortly. In the meantime, test out the app&nbsp;
-        <a
-          className="App-link"
-          href="https://monty.net/gratitude"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           here.
-        </a>
+          This site will be live shortly.
         </p>
       </header>
     </div>
