928d11a8dcf0dfb15d3a1440b984f6845820d39a
Monty Hood <montyhood@gmail.com>
2021-11-22 15:21:24 -0500

    updating content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to change this something.
+          App will be live shortly.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Gratitude card
+          Demo
         </a>
       </header>
     </div>
